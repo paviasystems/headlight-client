01b33b93d5a19f4b491bdf6f6cffc5590ded8a16
jasonhillier <jason@hillier.us>
2019-03-20 15:28:24 -0700

    increase timeout

--- node/test/spec.ts
+++ node/test/spec.ts
@@ -14,6 +14,8 @@ const _UserName = process.env['DEV_USER'] || 'user';
 const _Password = process.env['DEV_PASSWORD'] || 'password123';
 
 describe('Node API test', function() {
+    this.timeout(5000); //give network requests more time for build slaves
+
     var client = new Headlight.Client(_ServerURL);
 
     it('can get API', ()=>{
